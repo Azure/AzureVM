a17b0966e0f690630c9a5e2d548179f2a1c1e6e9
hong-revo <hongooi@microsoft.com>
2018-10-20 22:56:20 +1100

    change docType for added methods in documentation

--- R/add_methods.R
+++ R/add_methods.R
@@ -4,19 +4,23 @@
 #'
 #' Method for the [AzureRMR::az_subscription] class.
 #'
-#' @rdname list_vm_sizes
-#' @name list_vm_sizes
-#' @usage
+#' @docType class
+#' @section Usage:
+#' ```
 #' list_vm_sizes(location, name_only = FALSE)
+#' ```
+#' @section Arguments:
+#' - `location`: The location/region for which to obtain available VM sizes.
+#' - `name_only`: Whether to return only a vector of names, or all information on each VM size.
 #'
-#' @param location The location/region for which to obtain available VM sizes.
-#' @param name_only Whether to return only a vector of names, or all information on each VM size.
-#'
-#' @return
+#' @section Value:
 #' If `name_only` is TRUE, a character vector of names, suitable for passing to `create_vm`. If FALSE, a data frame containing the following information for each VM size: the name, number of cores, OS disk size, resource disk size, memory, and maximum data disks.
 #'
 #' @seealso
 #' [create_vm]
+#' @rdname list_vm_sizes
+#' @aliases list_vm_sizes
+#' @name list_vm_sizes
 NULL
 
 
@@ -24,23 +28,23 @@ NULL
 #'
 #' Method for the [AzureRMR::az_subscription] and [AzureRMR::az_resource_group] classes.
 #'
-#' @rdname get_vm
-#' @name get_vm
-#' @aliases get_vm_cluster list_vms
-#' @usage
+#' @docType class
+#' @section Usage:
+#' ```
 #' get_vm(name, resource_group = name)
 #' get_vm(name)
 #' get_vm_cluster(name, resource_group = name)
 #' get_vm_cluster(name)
 #' list_vms()
+#' ```
+#' @section Arguments:
+#' - `name`: The name of the VM or cluster.
+#' - `resource_group`: For the `az_subscription` method, the resource group in which `get_vm()` will look for the VM. Defaults to the VM name.
 #'
-#' @param name The name of the VM or cluster.
-#' @param resource_group For the `az_subscription` method, the resource group in which `get_vm()` will look for the VM. Defaults to the VM name.
-#'
-#' @details
+#' @section Details:
 #' Despite the names, `get_vm` and `get_vm_cluster` can both be used to retrieve individual VMs and clusters. The main difference is in their behaviour if a deployment template is not found. In the case of `get_vm`, it also searches for a raw VM resource of the given name, whereas `get_vm_cluster` will throw an error immediately.
 #'
-#' @return
+#' @section Value:
 #' For `get_vm()`, an object representing the VM, either of class `az_vm_template` or `az_vm_resource`.
 #'
 #' For `list_vms()`, a list of such objects.
@@ -50,6 +54,9 @@ NULL
 #' @seealso
 #' [az_vm_template], [az_vm_resource],
 #' [AzureRMR::az_subscription], [AzureRMR::az_resource_group]
+#' @rdname get_vm
+#' @aliases get_vm get_vm_cluster list_vms
+#' @name get_vm
 NULL
 
 
@@ -57,10 +64,9 @@ NULL
 #'
 #' Method for the [AzureRMR::az_subscription] and [AzureRMR::az_resource_group] classes.
 #'
-#' @rdname create_vm
-#' @name create_vm
-#' @aliases create_vm_cluster
-#' @usage
+#' @docType class
+#' @section Usage:
+#' ```
 #' create_vm(name, location, os = c("Windows", "Ubuntu"), size = "Standard_DS3_v2",
 #'           username, passkey, userauth_type = c("password", "key"),
 #'           ext_file_uris = NULL, inst_command = NULL,
@@ -69,23 +75,24 @@ NULL
 #'                   username, passkey, userauth_type = c("password", "key"),
 #'                   ext_file_uris = NULL, inst_command = NULL, clust_size,
 #'                   template, parameters, ..., wait = TRUE)
+#' ```
+#' @section Arguments:
+#' - `name`: The name of the VM or cluster.
+#' - `location`: The location for the VM. Use the `list_locations()` method of the `AzureRMR::az_subscription` class to see what locations are available.
+#' - `os`: The operating system for the VM.
+#' - `size`: The VM size. Use the `list_vm_sizes()` method of the `AzureRMR::az_subscription` class to see what sizes are available.
+#' - `username`: The login username for the VM.
+#' - `passkey`: The login password or public key.
+#' - `userauth_type`: The type of login authentication to use. Only has an effect for Linux-based VMs; Windows VMs will always use `"password"`.
+#' - `ext_file_uris`: Optional link to download extension packages.
+#' - `inst_command`: If `ext_file_uris` is supplied, the install script to run. Defaults to `install.sh` for an Ubuntu VM, or `install.ps1` for a Windows VM.
+#' - `clust_size`: For a cluster, the number of nodes to create.
+#' - `template`: Optional: the VM template to deploy. By default, this is determined by the values of the other arguments; see 'Details' below.
+#' - `parameters`: Optional: other parameters to pass to the deployment.
+#' - `wait`: Whether to wait until the deployment is complete.
+#' - `...`: Other arguments to lower-level methods.
 #'
-#' @param name The name of the VM or cluster.
-#' @param location The location for the VM. Use the `list_locations()` method of the `AzureRMR::az_subscription` class to see what locations are available.
-#' @param os The operating system for the VM.
-#' @param size The VM size. Use the `list_vm_sizes()` method of the `AzureRMR::az_subscription` class to see what sizes are available.
-#' @param username The login username for the VM.
-#' @param passkey The login password or public key.
-#' @param userauth_type The type of login authentication to use. Only has an effect for Linux-based VMs; Windows VMs will always use `"password"`.
-#' @param ext_file_uris Optional link to download extension packages.
-#' @param inst_command If `ext_file_uris` is supplied, the install script to run. Defaults to `install.sh` for an Ubuntu VM, or `install.ps1` for a Windows VM.
-#' @param clust_size For a cluster, the number of nodes to create.
-#' @param template Optional: the VM template to deploy. By default, this is determined by the values of the other arguments; see 'Details' below.
-#' @param parameters Optional: other parameters to pass to the deployment.
-#' @param wait Whether to wait until the deployment is complete.
-#' @param ... Other arguments to lower-level methods.
-#'
-#' @details
+#' @section Details:
 #' This method deploys a template to create a new virtual machine or cluster of VMs. Currently, seven templates are supplied with this package, based on the Azure Data Science Virtual Machine:
 #' - Ubuntu DSVM
 #' - Ubuntu DSVM using public key authentication
@@ -101,13 +108,16 @@ NULL
 #'
 #' For the `AzureRMR::az_subscription` method, this will by default create the VM in _exclusive_ mode, meaning a new resource group is created solely to hold the VM. This simplifies managing the VM considerably, in particular deleting the resource group will also automatically delete all the VM's resources.
 #'
-#' @return
+#' @section Value:
 #' An object of class `az_vm_template` representing the created VM.
 #'
 #' @seealso
 #' [az_vm_template],
 #' [AzureRMR::az_subscription], [AzureRMR::az_resource_group],
 #' [Data Science Virtual Machine](https://azure.microsoft.com/en-us/services/virtual-machines/data-science-virtual-machines/)
+#' @rdname create_vm
+#' @aliases create_vm create_vm_cluster
+#' @name create_vm
 NULL
 
 
@@ -115,23 +125,23 @@ NULL
 #'
 #' Method for the [AzureRMR::az_subscription] and [AzureRMR::az_resource_group] classes.
 #'
-#' @rdname delete_vm
-#' @name delete_vm
-#' @aliases delete_vm_cluster
-#' @usage
+#' @docType class
+#' @section Usage:
+#' ```
 #' delete_vm(name, confirm = TRUE, free_resources = TRUE)
 #' delete_vm(name, confirm = TRUE, free_resources = TRUE,
 #'           resource_group = name)
 #' delete_vm_cluster(name, confirm = TRUE, free_resources = TRUE)
 #' delete_vm_cluster(name, confirm = TRUE, free_resources = TRUE,
 #'                   resource_group = name)
+#' ```
+#' @section Arguments:
+#' - `name` The name of the VM or cluster.
+#' - `confirm` Whether to confirm the delete.
+#' - `free_resources` If this was a deployed template, whether to free all resources created during the deployment process.
+#' - `resource_group` For the `AzureRMR::az_subscription` method, the resource group containing the VM or cluster.
 #'
-#' @param name The name of the VM or cluster.
-#' @param confirm Whether to confirm the delete.
-#' @param free_resources If this was a deployed template, whether to free all resources created during the deployment process.
-#' @param resource_group For the `AzureRMR::az_subscription` method, the resource group containing the VM or cluster.
-#'
-#' @details
+#' @section Details:
 #' If the VM or cluster is of class [az_vm_template] and was created in exclusive mode, this method deletes the entire resource group that it occupies. This automatically frees all resources that were created during the deployment process. Otherwise, if `free_resources=TRUE`, it manually deletes each individual resource in turn. This is done synchronously (the method does not return until the deletion is complete) to allow for dependencies.
 #'
 #' If the VM is of class [az_vm_resource], this method only deletes the VM resource itself, not any other resources it may depend on.
@@ -139,6 +149,9 @@ NULL
 #' @seealso
 #' [create_vm], [az_vm_template], [az_vm_resource],
 #' [AzureRMR::az_subscription], [AzureRMR::az_resource_group]
+#' @rdname delete_vm
+#' @aliases delete_vm delete_vm_cluster
+#' @name delete_vm
 NULL
 
 
