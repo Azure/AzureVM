e929ea40bb718b625e0d50009026e5109f3786d0
Hong Ooi <hongooi@microsoft.com>
2019-06-07 18:27:07 +1000

    autoscaler

--- R/vmss_resource_config.R
+++ R/vmss_resource_config.R
@@ -87,12 +87,51 @@ lb_probe_jupyter <- lb_probe_config(8000)
 lb_probe_rstudio <- lb_probe_config(8787)
 
 
-autoscale_config <- function()
+autoscaler_config <- function(profiles=list())
 {
-
+    props <- list(profiles=profiles)
+    structure(list(properties=props), class="as_config")
 }
 
-scaleset_nic_config <- function()
-{
 
+autoscaler_profile_config <- function(name="Profile", minsize=1, maxsize=NA, default=NA, scale_out=0.75, scale_in=0.25,
+                                      interval="PT1M", window="PT5M")
+{
+    if(is.na(maxsize))
+        maxsize <- "[variables('asMaxCapacity')]"
+    if(is.na(default))
+        default <- "[parameters('instanceCount')]"
+    capacity <- list(minimum=minsize, maximum=maxsize, default=default)
+
+    trigger <- list(
+        metricName="Percentage CPU",
+        metricNamespace="",
+        metricResourceUri="[variables('vmId')]",
+        timeGrain=interval,
+        timeWindow=window,
+        statistic="Average",
+        timeAggregation="Average"
+    )
+    action <- list(
+        type="ChangeCount",
+        value="[variables('asScaleValue')]",
+        cooldown=interval
+    )
+
+    rule_out <- list(metricTrigger=trigger, scaleAction=action)
+    rule_out$metricTrigger$operator <- "GreaterThan"
+    rule_out$metricTrigger$threshold <- round(scale_out * 100)
+    rule_out$direction <- "Increase"
+
+    rule_in <- list(metricTrigger=trigger, scaleAction=action)
+    rule_in$metricTrigger$operator <- "LessThan"
+    rule_in$metricTrigger$threshold <- round(scale_in * 100)
+    rule_in$direction <- "Decrease"
+
+    prof <- list(
+        name=name,
+        capacity=capacity,
+        rules=c(rule_out, rule_in)
+    )
+    structure(prof, class="as_profile_config")
 }
